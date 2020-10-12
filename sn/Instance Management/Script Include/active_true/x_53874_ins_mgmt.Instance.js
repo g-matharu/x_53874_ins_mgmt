8f0ed805b35f5dbd8e4a8656d6f50fe9137d87f7
System Administrator <admin@example.com>
2020-10-13 04:33:48 +0530

    Changes by 'System Administrator' on 'GSM - Inst mgmt' -- Build #7

---
+++ sn/Instance Management/Script Include/active_true/x_53874_ins_mgmt.Instance.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Instance Management
+ * ClassName   : sys_script_include
+ * Created On  : 2020-10-12 22:06:12
+ * Created By  : admin
+ * Updated On  : 2020-10-12 22:06:12
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=ef6eb3f92fb710103fac57892799b6f6
+ */
+var Instance = Class.create();
+Instance.prototype = {
+    initialize: function() {
+    },
+
+    type: 'Instance'
+};
